1a04f6f327452547b90f94f8ec2e6653e84eba93
crmessitv090-cmd <crmessitv090@gmail.com>
2026-09-05 17:17:07 +0500

    Optimize email sending

--- src/java/util/EmailUtil.java
+++ src/java/util/EmailUtil.java
@@ -23,9 +23,13 @@ public class EmailUtil {
         Properties props = new Properties();
 
         props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
+props.put("mail.smtp.port", "587");
+props.put("mail.smtp.auth", "true");
+props.put("mail.smtp.starttls.enable", "true");
+
+props.put("mail.smtp.connectiontimeout", "5000");
+props.put("mail.smtp.timeout", "5000");
+props.put("mail.smtp.writetimeout", "5000");
 
         return Session.getInstance(
                 props,
